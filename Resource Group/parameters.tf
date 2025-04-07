ef3dc7be004dd1aab520aa66068a6fe2e0b0b26c
Nilnuwan Sankalpa <nilnuwans@jkintranet.com>
2025-04-07 11:14:41 +0530

    initial commit

---
+++ Resource Group/parameters.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group"
+  type        = string
+  default     = "example-rg"
+}
+
+variable "location" {
+  description = "The Azure region for the Resource Group"
+  type        = string
+  default     = "East US"
+}
+
+variable "tags" {
+  description = "Tags to apply to the Resource Group"
+  type        = map(string)
+  default = {
+    Environment = "Dev"
+    Owner       = "Team A"
+  }
+}
